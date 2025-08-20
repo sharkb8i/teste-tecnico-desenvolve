9a31e59e907f529850c6c5077857319e3b8f4097
Bruno Diedrich <brunodiedrich97@gmail.com>
2025-08-20 10:19:25 -0300

    chore(start): first commit

---
+++ backend/servidores/src/main/java/com/municipio/servidores/ServidoresApplication.java
@@ -0,0 +1,13 @@
+package com.municipio.servidores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidoresApplication.class, args);
+	}
+
+}
